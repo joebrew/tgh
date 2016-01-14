13aa2f71be546cfb67e791d481dda3ec4a0d4d1c
Joe Brew <joebrew@gmail.com>
2016-01-14 14:49:16 +0100

    Atlas now reshifted away from malaria and towards general health/wealth

--- protocols/atlas_of_malaria_control/helpers.R
+++
@@ -1,7 +0,0 @@
-library(ggplot2)
-library(ggthemes)
-theme_gg <- theme_economist() +
-  theme(plot.title = element_text(size = 12),
-        axis.title.x = element_text(size = 8),
-        axis.title.y = element_text(size = 8),
-        legend.text = element_text(size = 6))
